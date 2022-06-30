fadba6a84e6ac3974fb7cb8cc42e7ea58e613b28
fsanntiago <zerofabricio@hotmail.com>
2022-06-30 17:33:57 -0300

    feat: Add theme and fonts

---
+++ App.tsx
@@ -0,0 +1,58 @@
+import React, { useCallback, useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { ThemeProvider } from "styled-components";
+
+import { Home } from "./src/screen/Home";
+
+import theme from "./src/styles/theme";
+
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          Archivo_400Regular,
+          Archivo_500Medium,
+          Archivo_600SemiBold,
+          Inter_400Regular,
+          Inter_500Medium,
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <GestureHandlerRootView onLayout={onLayoutRootView} style={{ flex: 1 }}>
+      <ThemeProvider theme={theme}>
+        <Home />
+      </ThemeProvider>
+    </GestureHandlerRootView>
+  );
+}
